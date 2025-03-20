7ef8b3e87640054652f5760f132cc6d5cc29f8a9
Javier <xavimateosm@gmail.com>
2025-03-20 14:24:51 +0100

    Added class for compound loss usage

---
+++ losses/combined_loss.py
@@ -0,0 +1,14 @@
+import torch
+from torch.nn import L1Loss
+from monai.losses import SSIMLoss
+
+class CombinedLoss(torch.nn.module):
+    def __init__(self, alpha=0.5, beta=0.5):
+        super().__init__()
+        self.l1 = L1Loss()
+        self.ssim = SSIMLoss(spatial_dims=3)
+        self.alpha = alpha
+        self.beta = beta
+
+    def forward(self, pred, target):
+        return self.alpha * self.l1(pred, target) + self.beta * self.ssim(pred, target)
